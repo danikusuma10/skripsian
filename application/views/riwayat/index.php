b4157ff518969b5c969b2face5a8b1815c13bff4
danikusuma10 <kusumadanikusuma@outlook.com>
2021-07-01 13:16:54 +0700

    footer

--- application/views/riwayat/index.php
+++ application/views/riwayat/index.php
@@ -27,6 +27,16 @@
             
 </form>
         
+<footer class="sticky-footer bg-white">
+     <div class="container my-auto">
+         <div class="copyright text-center my-auto">
+             <span> YPT's - <em>Payment </em><?= date('Y'); ?> <em class="badge badge-info"> prototype</em> | <em>Developed By</em> Dani | <em>Theme by</em> <a href="https://startbootstrap.com/themes/sb-admin-2"> SB Admin 2</a>
+             </span>
+         </div>
+     </div>
+ </footer> <!-- End of Footer -->
+
+
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
