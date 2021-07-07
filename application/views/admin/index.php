467f3bd00fb1f7cef58c440037bceeb344a46b36
danikusuma10 <kusumadanikusuma@outlook.com>
2021-07-07 15:02:01 +0700

    a

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -48,12 +48,12 @@
 
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
-        <?php if ($_SESSION["role_id"] == "1") { ?><a href="<?= base_url('transaksi/kasmasuk'); ?>"><?php } ?>
+        <?php if ($_SESSION["role_id"] == "1") { ?><a href="<?= base_url('laporan'); ?>"><?php } ?>
             <div class="card border-left-info shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Kas Masuk</div>
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Saldo Kas Masuk</div>
                             <div class="row no-gutters align-items-center">
                                 <div class="col-auto">
                                     <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">Rp <?= number_format($kas_masuk['nominal']) ?></div>
@@ -71,12 +71,12 @@
 
         <!-- Pending Requests Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
-        <?php if ($_SESSION["role_id"] == "1") { ?><a href="<?= base_url('transaksi/kaskeluar'); ?>"><?php } ?>
+        <?php if ($_SESSION["role_id"] == "1") { ?><a href="<?= base_url('laporan'); ?>"><?php } ?>
             <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Kas Keluar</div>
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Saldo Kas Keluar</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">Rp <?= number_format($kas_keluar['nominal']) ?></div>
                         </div>
                         <div class="col-auto">
