4871a3911bb74e60bf4345692a37c4582d8d920a
danikusuma10 <kusumadanikusuma@outlook.com>
2021-07-08 21:45:51 +0700

    notif

---
+++ application/controllers/Notifications.php
@@ -0,0 +1,50 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Notifications extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+
+
+	public function __construct()
+    {
+        parent::__construct();
+        $params = array('server_key' => 'SB-Mid-server-agj15d5qnNn06ZuKmkPA785C', 'production' => false);
+		$this->load->library('veritrans');
+		$this->veritrans->config($params);
+		$this->load->helper('url');
+		
+    }
+
+	public function index()
+	{
+		echo 'test notification handler';
+		$json_result = file_get_contents('php://input');
+		$result = json_decode($json_result);
+
+		if($result){
+		$notif = $this->veritrans->status($result->order_id);
+		}
+
+		error_log(print_r($result,TRUE));
+
+		//notification handler sample
+
+		
+		
+
+	}
+}
