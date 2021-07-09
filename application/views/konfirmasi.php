5e84997e95880bb4a32d81ba13243b05611bd8bd
danikusuma10 <kusumadanikusuma@outlook.com>
2021-07-09 13:29:45 +0700

    a

--- application/views/konfirmasi.php
+++ application/views/konfirmasi.php
@@ -1,19 +1,71 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
 
-<link rel="stylesheet" media="screen" href="<?= base_url('assets'); ?>particle/style.css">
+	<style type="text/css">
 
-<script src="<?= base_url('assets'); ?>particle/particles.js"></script>
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
 
-<script src="<?= base_url('assets'); ?>particle/app.js"></script>
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
 
+	a {
+		color: #003399;
+		background-color: transparent;
+		font-weight: normal;
+	}
 
-<div id="particles-js">
+	h1 {
+		color: #444;
+		background-color: transparent;
+		border-bottom: 1px solid #D0D0D0;
+		font-size: 19px;
+		font-weight: normal;
+		margin: 0 0 14px 0;
+		padding: 14px 15px 10px 15px;
+	}
 
-  <div class="banner" style="width: 500px;background: rgba(255,255,255,0.5); position: absolute; top:40%;left:30%;text-align: center;padding: 20px">
+	code {
+		font-family: Consolas, Monaco, Courier New, Courier, monospace;
+		font-size: 12px;
+		background-color: #f9f9f9;
+		border: 1px solid #D0D0D0;
+		color: #002166;
+		display: block;
+		margin: 14px 0 14px 0;
+		padding: 12px 10px 12px 10px;
+	}
 
-    <h1>Membuat Background Animasi Bergerak Menggunakan Particle.js</h1>
+	#body {
+		margin: 0 15px 0 15px;
+	}
 
-  </div>
-  <body>
+	p.footer {
+		text-align: right;
+		font-size: 11px;
+		border-top: 1px solid #D0D0D0;
+		line-height: 32px;
+		padding: 0 10px 0 10px;
+		margin: 20px 0 0 0;
+	}
+
+	#container {
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		box-shadow: 0 0 8px #D0D0D0;
+	}
+	</style>
+</head>
+<body>
 
 <div id="container">
 	<h1>Confirmation</h1>
@@ -103,233 +155,4 @@
 </div>
 
 </body>
-</div>
-
-
-
-
-<script>
-
-  /* ---- particles.js config ---- */
-
-particlesJS("particles-js", {
-
-  "particles": {
-
-    "number": {
-
-      "value": 100,
-
-      "density": {
-
-        "enable": true,
-
-        "value_area": 800
-
-      }
-
-    },
-
-    "color": {
-
-      "value": "#ffffff"
-
-    },
-
-    "shape": {
-
-      "type": "circle",
-
-      "stroke": {
-
-        "width": 2,
-
-        "color": "#ffffff"
-
-      },
-
-      "polygon": {
-
-        "nb_sides": 5
-
-      },
-
-      "image": {
-
-        "src": "img/github.svg",
-
-        "width": 100,
-
-        "height": 100
-
-      }
-
-    },
-
-    "opacity": {
-
-      "value": 0.5,
-
-      "random": false,
-
-      "anim": {
-
-        "enable": false,
-
-        "speed": 1,
-
-        "opacity_min": 0.1,
-
-        "sync": false
-
-      }
-
-    },
-
-    "size": {
-
-      "value": 50,
-
-      "random": true,
-
-      "anim": {
-
-        "enable": false,
-
-        "speed": 40,
-
-        "size_min": 0.1,
-
-        "sync": false
-
-      }
-
-    },
-
-    "line_linked": {
-
-      "enable": true,
-
-      "distance": 150,
-
-      "color": "#ffffff",
-
-      "opacity": 0.4,
-
-      "width": 1
-
-    },
-
-    "move": {
-
-      "enable": true,
-
-      "speed": 6,
-
-      "direction": "none",
-
-      "random": false,
-
-      "straight": false,
-
-      "out_mode": "out",
-
-      "bounce": false,
-
-      "attract": {
-
-        "enable": false,
-
-        "rotateX": 600,
-
-        "rotateY": 1200
-
-      }
-
-    }
-
-  },
-
-  "interactivity": {
-
-    "detect_on": "canvas",
-
-    "events": {
-
-      "onhover": {
-
-        "enable": true,
-
-        "mode": "repulse"
-
-      },
-
-      "onclick": {
-
-        "enable": true,
-
-        "mode": "push"
-
-      },
-
-      "resize": true
-
-    },
-
-    "modes": {
-
-      "grab": {
-
-        "distance": 140,
-
-        "line_linked": {
-
-          "opacity": 1
-
-        }
-
-      },
-
-      "bubble": {
-
-        "distance": 400,
-
-        "size": 40,
-
-        "duration": 2,
-
-        "opacity": 8,
-
-        "speed": 3
-
-      },
-
-      "repulse": {
-
-        "distance": 200,
-
-        "duration": 0.4
-
-      },
-
-      "push": {
-
-        "particles_nb": 4
-
-      },
-
-      "remove": {
-
-        "particles_nb": 2
-
-      }
-
-    }
-
-  },
-
-  "retina_detect": true
-
-});
-
-</script>
+</html>
