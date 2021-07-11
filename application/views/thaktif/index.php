b3e7755541f4bd630e6f3e54c561665d386c834d
danikusuma10 <kusumadanikusuma@outlook.com>
2021-07-12 00:19:35 +0700

    as

--- application/views/thaktif/index.php
+++ application/views/thaktif/index.php
@@ -85,7 +85,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="addNewTahunaktifLabel">Tambah Admin Baru</h5>
+                <h5 class="modal-title" id="addNewTahunaktifLabel">Tambah Tahun Aktif</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
