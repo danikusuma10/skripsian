dfa0fc1d1023f0b94bc052b5ded2885efad5720d
danikusuma10 <kusumadanikusuma@outlook.com>
2021-07-11 22:31:53 +0700

    ass

--- application/views/siswa/index.php
+++ application/views/siswa/index.php
@@ -116,12 +116,12 @@
 <!-- edit Modal-->
 <?php foreach ($siswa as $s) : ?>
     <div class="modal fade" id="editModal<?= $s['id_bayar']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-dialog modal" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Edit Data Siswa</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
+                        <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
 
