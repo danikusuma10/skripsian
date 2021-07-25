2aee4b85b25132e34ef0a90ef7d1d36c2b1618fa
danikusuma10 <kusumadanikusuma@outlook.com>
2021-07-25 14:07:17 +0700

    a

--- application/views/riwayat/index.php
+++ application/views/riwayat/index.php
@@ -47,11 +47,11 @@
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
                 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-                <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+                <script src="<?= base_url('assets'); ?>vendor/datatables/jquery.dataTables.min.js"></script>
+               
 
 
-
-                <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+              
 
 
 
